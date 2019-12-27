5efb5f34662c054a26c3b10dca2254dbdf17661a
jachumail <jachumail@yahoo.pl>
2019-12-27 12:03:26 +0100

    random threads

--- MatrixConsole/Program.cs
+++ MatrixConsole/Program.cs
@@ -10,7 +10,7 @@ namespace MatrixConsole
      
     class Program
     {
-        DateTime dateTime = new DateTime();
+        DateTime startTime = new DateTime();
         static string charDatabase = "1";
         static string OneChar()
         {
@@ -23,18 +23,38 @@ namespace MatrixConsole
             Console.SetWindowPosition(0, 0);
             int windowWidth = Console.WindowWidth;
             int windowHeight = Console.WindowHeight;
-            //int windowHeight = Console./2;
-            //Console.SetWindowSize(windowWidth, windowHeight);
-            //THREAD
-            //for (int i = 0; i <10; i++)
+
+            DateTime start = new DateTime();
+
+            int i = 0;
             while (true)
             {
                 Thread t = new Thread(new ThreadStart(ThreadLine));
                 t.Start();
-                Thread.Sleep(300 );
+                Thread.Sleep(100);
+                i++;
+                if(i> 100) { 
+                    Thread.Sleep(100);
+                    Thread cc = new Thread(new ThreadStart(CleanConsole));
+                    cc.Start();
+                    i = 0;
+                }
             }
 
             Console.ReadKey();
+        }
+        static void CleanConsole()
+        {
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+            for (int i = 0; i < width; i+=3)
+            {
+                for(int j =0; j< height; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.WriteLine("   ");
+                }
+            }
         }
         static void ThreadLine(){
 
@@ -47,7 +67,7 @@ namespace MatrixConsole
 
             int height = rnd.Next(5, 45);
 
-            int timeToClean = rnd.Next(4500, 5500);
+            int timeToClean = rnd.Next(1500, 2500);
 
             for (int j = 2; j < height; j++)
             {
@@ -55,23 +75,24 @@ namespace MatrixConsole
                     break;
                 Console.SetCursorPosition(posX, j + posY - 1);
                 Console.ForegroundColor = ConsoleColor.Green;
+                
                 Console.Write(OneChar());
                 
-                Thread.Sleep(1);
+                Thread.Sleep(rnd.Next(2, 5));
                 
                 Console.SetCursorPosition(posX, j + posY);
-                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write(head);
 
-                Thread.Sleep(50);
+                Thread.Sleep(rnd.Next(2, 5));
             }
             Thread.Sleep(timeToClean);
             for(int i = 0; i< height; i++)
             {
-                if (i + posY > Console.WindowHeight || posX > Console.WindowWidth) break;
+                if (i + posY > Console.WindowHeight || posX > Console.WindowWidth || posX -3 < 0) break;
 
-                Console.SetCursorPosition(posX, i + posY+1);
-                Console.Write(" ");
+                Console.SetCursorPosition(posX-2 , i + posY+1);
+                Console.Write("       ");
 
                 Thread.Sleep(50);
             }
